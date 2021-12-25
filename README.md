9d3708b44620de54f08fded01a224e7eb8afcfd6
jayakhan <jayakhan04@gmail.com>
2021-12-25 04:33:28 -0500

    modified ngram model

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# markov-text-generator
+#### In this project, we have created a trigram model on austen-sense corpus. The sentence gets generated based on Markov assumption - we look at just last two words to predict the next word in the sentence. The next word is selected based on its frequency along with last two words in the corpus.
+
+#### Below are few sentences that get generated. 
+
+*right to come to see them, and the two miss steeles, as she had*
+
+*she was not in the world.*
+
+*do not know what you are very much to do.*
+
+*should hardly call her a little, or any of their being together, and the*
+
--- mtg.py
+++ mtg.py
@@ -1,86 +1,56 @@
-from collections import defaultdict
+"""Markov Text Generator.
+
+Jaya Khan, 2021
+"""
+
 import nltk
-import random
 
-#Return extended sentence from training corpus
-def pick_sentence(corpus, word):
-    punctuations = ['.', '?', '!']
-    sentence = ''
-    for i in range(len(corpus)):
-        corpus_word = ' '.join(corpus[i : i + len(word.split(' '))])
-        if  corpus_word == word:
-            for element in range(i + len(word.split(' ')), len(corpus)):
-                while len(sentence.split(' ')) < 16 and corpus[i] not in punctuations:
-                    sentence += ''.join(corpus[i]) + ' '
-                    i += 1
-                break
-            break
-    return sentence.replace(" ,",",")
 
-#Create probability distribution by frequency of words appeared in training corpus
-def calculate_probability(model, total_count):
-    for key, value in model.items():
-        model[key] = value/total_count
-
-#Pick random word by weight from model
-def pick_random_word(model):
-    word_backoff = []
-    word_prob = []
-    for key, value in model.items():
-        word_backoff.append(key) 
-        word_prob.append(value)
-    return random.choices(word_backoff, word_prob)
-
-#Create language model
-def create_model(count, model, sentence, n, corpus):
-    for i in range(len(corpus)):
-        if ' '.join(corpus[i : i + n]) == ' '.join(sentence):
-            count += 1
-            model[' '.join(corpus[i : i + n + 1])] += 1 
-    return count
+def initialize_corpus(n, corpus):
+    tokens = (n-1)*['<START>']+corpus
+    m = [(tuple([tokens[i-p-1] for p in reversed(range(n-1))]), tokens[i]) for i in range(n-1, len(tokens))]
+    return m
 
 def finish_sentence(sentence, n, corpus, deterministic=False):
-    model = defaultdict(lambda:0)
-    count = 0
-    punctuations = ['.', '?', '!']
-    if not any(char in punctuations for char in sentence):
-        if deterministic:
-            if len(sentence) == n:
-                total_count = create_model(count, model, sentence, n, corpus)
-                if total_count > 0:
-                    calculate_probability(model, total_count)
-                    return pick_sentence(corpus, max(model, key= lambda x: model[x]))
-                else: 
-                    n = n - 1
-                    while(n > 0):
-                        return finish_sentence(sentence[:-1], n, corpus, deterministic)
-                    return "found no match"
-            else: return 'please select right n gram model'
-        else:
-            if len(sentence) == n:
-                total_count = create_model(count, model, sentence, n, corpus)
-                if total_count > 0:
-                    calculate_probability(model, total_count)
-                    random_word = pick_random_word(model)
-                    return pick_sentence(corpus, ''.join(random_word))
-                else: 
-                    n = n - 1
-                    while(n > 0):
-                        return finish_sentence(sentence[:-1], n, corpus, deterministic)
-                    return "found no match"
-            else: return 'please select right n gram model'
-    else: return "word list contains punctuations"
+    model = initialize_corpus(n, corpus)
+    if deterministic:
+        ngram_counter = {}
+        context = {}
+        punctuations = ['.', '?', '!']
 
+        for ngram in model:
+            if ngram in ngram_counter:
+                ngram_counter[ngram] += 1
+            else:
+                ngram_counter[ngram] = 1
+            prev_words, target_word = ngram
+            if prev_words in context:
+                context[prev_words].append(target_word)
+            else:
+                context[prev_words] = [target_word]
+
+        while len(sentence) < 16:
+            test_dict = {}
+            history = [s for s in sentence[-n+1:]]
+            next_tokens = context[tuple(history)]
+            for token in next_tokens:
+                test_dict[token] = ngram_counter[(tuple(history), token)]
+            sentence.append(max(test_dict, key= lambda x: test_dict[x]))
+            if any(char in punctuations for char in sentence):
+                break
+        return sentence
+
+                
 
 if __name__ == "__main__":
     nltk.download('gutenberg')
     nltk.download('punkt')
     corpus = nltk.corpus.gutenberg.raw('austen-sense.txt')
     corpus = nltk.word_tokenize(corpus.lower())
-
+    
     words = finish_sentence(
-        ['190', '2'],
-        2,
+        ['she', 'was', 'not'],
+        3,
         corpus,
         deterministic=True,
     )
